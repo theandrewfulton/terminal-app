0d4c8532f9e5500adfb18f04879ad985216a4b72
theandrewfulton <zenithsprings1@yahoo.com.au>
2020-12-13 13:48:55 +1100

    add artii gem for welcome message

--- src/index.rb
+++ src/index.rb
@@ -1,12 +1,14 @@
 require "tty-prompt"
+require "artii"
 
 # Welcome message
+welcome_art = Artii::Base.new
+puts welcome_art.asciify ('Daily Tasks')
 # create welcome menu variable
 @prompt = TTY::Prompt.new
 
-
 def welcome_menu
-user_input = @prompt.select("What would you like to do?") do |menu|
+user_input = @prompt.select("\n What would you like to do?\n") do |menu|
     menu.choice "Create a new task to track", 0
     menu.choice "Select an existing task to track", 1
     menu.choice "Show a task visually", 2
