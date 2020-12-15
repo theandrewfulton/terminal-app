a4ea83b60181bba0c50f997899da67e6ba913f39
theandrewfulton <zenithsprings1@yahoo.com.au>
2020-12-15 19:15:00 +1100

    create class to modify a task, along with with changes to testing

--- src/spec/daily_task_spec.rb
+++ src/spec/daily_task_spec.rb
@@ -1,5 +1,7 @@
 require_relative '../new_task'
 require_relative '../add_date'
+require_relative '../modify_task'
+
 
 describe AddDate do
     it 'should add a date the task was completed' do
@@ -14,5 +16,14 @@ describe NewTask do
         name = "Make Bed"
         new_task = NewTask.new(name)
         expect(new_task.name).to eq(name)
+    end
+end
+
+describe ModifyTask do
+    it 'should modify an existing task' do
+        date = "14/12"
+        task = "Make Bed"
+        # create new task
+        # add date to new task
     end
 end
