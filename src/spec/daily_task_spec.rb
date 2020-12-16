ed1e1a38c68fe60a07f6625e563b8e15be5b25c5
theandrewfulton <zenithsprings1@yahoo.com.au>
2020-12-16 19:49:29 +1100

    add bundler and rspec and prepare spec file

---
+++ src/spec/daily_task_spec.rb
@@ -0,0 +1,28 @@
+require_relative '../index'
+
+# describe AddDate do
+#     it 'should add a date the task was completed' do
+#         date = "14/12"
+#         add_date = AddDate.new(date)
+#         expect(add_date.date).to eq(date)
+#     end
+# end
+
+# describe NewTask do
+#     it 'should create a new task to track' do
+#         name = "Make Bed"
+#         new_task = NewTask.new(name)
+#         expect(new_task.name).to eq(name)
+#     end
+# end
+
+# describe ModifyTask do
+#     it 'should modify an existing task' do
+#         date = "14/12"
+#         task = "Make Bed"
+#         # create new task
+#         # add date to new task
+#     end
+# end
+
+# new_task
