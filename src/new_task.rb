d0db816da8f7460718039d5327e6caddcd694de2
theandrewfulton <zenithsprings1@yahoo.com.au>
2020-12-16 18:27:36 +1100

    move mvp contents to src and old files into a subdirectory

--- src/new_task.rb
+++ src/new_task.rb
@@ -1,14 +1,12 @@
-require_relative './add_date.rb'
-# The commented out code below works but I'm worried it's too
-# simplistic and won't actually achieve what I need it to.
-# I'm wondering if I need to create a third class to store and modify tasks
-class NewTask
-    attr_reader :name
-    def initialize(name)
-        @name = name
+    puts "What would you like to call this task?"
+    puts "Please make it short and easy to remember"
+    task_name = gets.chomp.gsub!(' ', '_')
+    #does file already exist?
+    if File.exist?('#{task_name}.txt') == false
+        # create new file
+        File.new('#{task_name}.txt', "w+")
+        puts "Hooray, you're ready to #{task_name} every day"
+        # options
+    else
+        puts "Sorry, there is already a task with this name"
     end
-end
-
-# create object
-# create a text file with name and spaces replaced with underscores
-# add the name to the text file
