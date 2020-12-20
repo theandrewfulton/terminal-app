0557db7eff3b01d5076410e0150c09c35e9031ae
theandrewfulton <zenithsprings1@yahoo.com.au>
2020-12-20 20:57:15 +1100

    new command line arguments and documentation. Proof-read of documentation

--- src/index.rb
+++ src/index.rb
@@ -12,15 +12,25 @@ require 'pastel'
 
 # Help method
 def help
-message_text("The information in this section can also be accessed with the commands: ```./install-run.sh --help``` and ```./install-run.sh -h```
-
-Navigate through the menus using the arrow keys. Then use Enter to make a selection.
+message_text("Navigate through the menus using the arrow keys. Then use Enter to make a selection.
 
 When it comes to inputting dates, the application parses your input into ISO8601 format. It is rather good, however for the best experience please enter dates in the format 'YYYY-MM-DD'
 
 Some prompts may require you to answer Yes or No. In this case, you will need to type Y for Yes or N for No, followed by Enter.
 
-If you have any questions or need help, please contact the developer on Twitter at [twitter.com/theandrewfulton](https://twitter.com/theandrewfulton)
+The following command line prompts can also be used as a shortcut to the selection menu of each feature:
+- -n, --new
+    - Opens a dialogue allowing the user to add a new task to track. Once confirmed, opens the main menu.
+- -c, --complete
+    - Opens a dialogue allowing the user to select a task to mark as complete and prompts for the date. Application exits.
+- -v, --view
+    - Opens a dialogue allowing the user to select a task to see visualised. The calendar view is then shown. When the user is finished the application exits.
+- -d, --delete
+    - Opens a dialogue allowing the user to select a task to delete. Once confirmed, application exits.
+- -h , --help
+    - Displays this help section
+
+If you have any questions or need help, please see the README located in the application's main directory or contact the developer on Twitter at https://twitter.com/theandrewfulton
 
 Thank you and have fun!")
 end
